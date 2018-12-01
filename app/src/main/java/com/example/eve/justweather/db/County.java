bbb44b1e7a927456b6aa97177d2f209b0579d0ba
choujiang <choujiang@126.com>
2018-12-01 17:52:31 +0800

    加入建立数据库和表的各项配置

---
+++ app/src/main/java/com/example/eve/justweather/db/County.java
@@ -0,0 +1,43 @@
+package com.example.eve.justweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    //为什么weatherId的类型是String？
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
